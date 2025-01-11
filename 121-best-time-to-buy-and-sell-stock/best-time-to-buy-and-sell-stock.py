06f4151d780cbceb1528a43813daa574e5b7731d
Ananya Bajaj <121599846+ananya-bajaj-DS@users.noreply.github.com>
2025-01-11 12:03:05 +0530

    Time: 142 ms (12.89%) | Memory: 19.1 MB (29.72%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,38 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+
+        l=0
+        r=1
+        maxP=0
+
+        while r<len(prices):
+            # print(prices[l] ,prices[r])
+            if prices[l] > prices[r]:
+                l=r
+                r+=1
+            else:
+                profit = prices[r] - prices[l]
+                maxP = max(maxP, profit)
+                r+=1
+        return maxP
+        
+
+
+ 
+        
+
+
+
+
+
+
+            
+
+
+        
+          
+        
