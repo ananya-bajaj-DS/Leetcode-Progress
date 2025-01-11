58bb0dc6aa5a51e2cb4651b61ed5abe9bb55e937
Ananya Bajaj <121599846+ananya-bajaj-DS@users.noreply.github.com>
2025-01-11 12:04:32 +0530

    Time: 88 ms (48.92%) | Memory: 19.1 MB (44.18%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -13,13 +13,13 @@ class Solution(object):
             # print(prices[l] ,prices[r])
             if prices[l] > prices[r]:
                 l=r
-                r+=1
+                # r+=1
             else:
                 profit = prices[r] - prices[l]
                 if maxP<profit:
                     maxP=profit
                 # maxP = max(maxP, profit)
-                r+=1
+            r+=1
         return maxP
         
 
