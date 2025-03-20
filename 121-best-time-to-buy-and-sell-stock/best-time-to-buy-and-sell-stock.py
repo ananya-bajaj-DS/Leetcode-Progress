68985d34992442bb9f33d4f97bf73ade70a30b20
Ananya Bajaj <121599846+ananya-bajaj-DS@users.noreply.github.com>
2025-03-20 17:11:17 +0530

    Time: 112 ms (39.32%) | Memory: 19.6 MB (11.32%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -5,22 +5,41 @@ class Solution(object):
         :rtype: int
         """
 
-        l=0
-        r=1
-        maxP=0
+        # l=0
+        # r=1
+        # maxP=0
 
-        while r<len(prices):
-            # print(prices[l] ,prices[r])
-            if prices[l] > prices[r]:
-                l=r
-                # r+=1
+        # while r<len(prices):
+        #     # print(prices[l] ,prices[r])
+        #     if prices[l] > prices[r]:
+        #         l=r
+        #         # r+=1
+        #     else:
+        #         profit = prices[r] - prices[l]
+        #         if maxP<profit:
+        #             maxP=profit
+        #         # maxP = max(maxP, profit)
+        #     r+=1
+        # return maxP
+
+        l=0
+        h=0
+        max_diff=0
+
+        if len(prices) <=1:
+            return 0
+
+        for h in range(1,len(prices)):
+            if l>=len(prices):
+                return max_diff
+            if prices[h] > prices[l]:
+                diff = prices[h] - prices[l]
+                max_diff = max(max_diff, diff)
+             
             else:
-                profit = prices[r] - prices[l]
-                if maxP<profit:
-                    maxP=profit
-                # maxP = max(maxP, profit)
-            r+=1
-        return maxP
+                l = h
+        return max_diff
+
         
 
 
